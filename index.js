fa782c15d7102a03628df3642ceffee736bd0cb2
Csomos Csaba-Attila <csomos.csaba99@gmail.com>
2020-12-17 15:38:28 +0200

    NodeJS project initialised

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/static/homepage.html');
+});
+
+
+
+app.listen(8080, () => {
+     console.log('Server listening on http://localhost:8080/ ...');
+});
