46fda8b36fc33b218f91b742b8b5be6d90bc394d
Csomos Csaba-Attila <csomos.csaba99@gmail.com>
2021-01-14 21:24:50 +0200

    fixed port for heroku

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 
 const app = express();
 
-
 app.get('/', (req, res) => {
   res.status(200).send('absdasdasd');
 });
 
 
-app.listen(8080, () => {
-  console.log('Server listening on http://localhost:8080/ ...');
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on port ${PORT} ...`);
 });
