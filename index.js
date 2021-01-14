d55f316dda8f79049c8a28434fa4252c6aee457f
Csomos Csaba-Attila <csomos.csaba99@gmail.com>
2021-01-14 08:40:04 +0200

    Added eslint

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/static/homepage.html');
+  res.status(200).sendFile(__dirname + '/static/homepage.html');
 });
 
 
-
 app.listen(8080, () => {
-     console.log('Server listening on http://localhost:8080/ ...');
+  console.log('Server listening on http://localhost:8080/ ...');
 });
