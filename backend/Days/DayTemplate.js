27b411a3dfe6e10a4e05e40619a59c5c4aed19dc
Csomos Csaba-Attila <csomos.csaba99@gmail.com>
2022-10-26 11:36:19 +0300

    Created a template for future use

---
+++ backend/Days/DayTemplate.js
@@ -0,0 +1,50 @@
+/**
+ * This is a file template for the day problems.
+ * Both for part 1 and part 2 of the problem.
+ */
+
+ const fs = require('fs');
+ const os = require('os');
+ 
+ const description = 'Lorem ipsum solor sit damet!';
+ const link = 'https://adventofcode.com/2020/day/';
+ 
+ /**
+  *
+  * @param {...} 
+  * @return {String} 
+  */
+ function solve() {
+  
+ }
+ 
+ /**
+  *  This function solves the second part of the problem from Day 1
+  *
+  * @param {...} 
+  * @return {String} 
+  */
+ function solve2() {
+ 
+}
+ 
+ /**
+  *
+  * @return {String} Returns solution for this days problem
+  */
+ function getSolution() {
+ }
+ 
+ /**
+  *
+  * @return {String} Returns solution for this days second part problem
+  */
+  function getSolution2() {
+  }
+ 
+ module.exports = {
+   description,
+   link,
+   getSolution,
+   getSolution2};
+ 
