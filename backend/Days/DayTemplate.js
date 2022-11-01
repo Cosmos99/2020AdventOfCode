fd0401030fa565230ae772cfafe9786190692f79
Csomos Csaba-Attila <csomos.csaba99@gmail.com>
2022-11-01 10:48:46 +0200

    Took out DayTemplate from eslint scope

--- backend/Days/DayTemplate.js
+++ backend/Days/DayTemplate.js
@@ -1,57 +1,55 @@
+/* eslint-disable */
+
 /**
  * This is a file template for the day problems.
  * Both for part 1 and part 2 of the problem.
  */
 
- const fs = require('fs');
- const os = require('os');
- 
- const description = 'Lorem ipsum solor sit damet!';
- const link = 'https://adventofcode.com/2020/day/';
- 
- /**
+const fs = require('fs');
+const os = require('os');
+
+const description = 'Lorem ipsum solor sit damet!';
+const link = 'https://adventofcode.com/2020/day/';
+
+/**
   *
-  * @param {...} 
-  * @return {String} 
+  * @param {...}
+  * @return {String}
   */
- function solve() {
-  
- }
- 
- /**
+function solve() {
+
+}
+
+/**
   *  This function solves the second part of the problem from Day 1
   *
-  * @param {...} 
-  * @return {String} 
+  * @param {...}
+  * @return {String}
   */
- function solve2() {
- 
+function solve2() {
+
 }
- 
- /**
+
+/**
   *
   * @return {String} Returns solution for this days problem
   */
- function getSolution() {
-
-    const lines = fs.readFileSync('./backend/Resources/dayX.txt', 'utf-8')
+function getSolution() {
+  const lines = fs.readFileSync('./backend/Resources/dayX.txt', 'utf-8')
       .split(os.EOL).filter(Boolean);
+}
 
- }
- 
- /**
+/**
   *
   * @return {String} Returns solution for this days second part problem
   */
-  function getSolution2() {
-
-    const lines = fs.readFileSync('./backend/Resources/dayX.txt', 'utf-8')
+function getSolution2() {
+  const lines = fs.readFileSync('./backend/Resources/dayX.txt', 'utf-8')
       .split(os.EOL).filter(Boolean);
-  }
- 
- module.exports = {
-   description,
-   link,
-   getSolution,
-   getSolution2};
- 
+}
+
+module.exports = {
+  description,
+  link,
+  getSolution,
+  getSolution2};
